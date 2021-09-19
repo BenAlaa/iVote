1587b2ec20a429008fa3a92073b27c754fb63e52
BenAlaa <ahmed.alaa0794@gmail.com>
2021-09-20 00:50:32 +0200

    feat/add Limited Route

---
+++ src/Routes/limitedRoute.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Redirect, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+
+
+export default ({component: Component, ...rest}) => {
+  const authedUser = useSelector(state => state.authedUser);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authedUser ? <Redirect to="/" /> : <Component {...props} /> 
+      }
+    />
+  )
+}
